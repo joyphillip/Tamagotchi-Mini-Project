fb4a70766affa9417bd95ebc43847229764a70e1
JPhillip018 <jphillip018@gmail.com>
2022-07-05 21:38:34 -0400

    created Tamagotchi class

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//console.log("Hello")
+// My Tamagotchi Game 
+// Create a Class (look at your notes on JS Classes if you forgot) for your Tamagotchi
+
+class Tamagotchi {
+    constructor (name, hunger = 1, sleepiness = 1, boredom = 1, age) {
+        this.name = name;
+        this.hunger = hunger;
+        this.sleepiness = sleepiness;
+        this.boredom = boredom;
+        this.age = age;
+    }
+    deadT(){
+        if (this.hunger >= 10 || this.sleepiness >= 10 || this.boredom >= 10){
+            alert( `${this.name} has died. GAME OVER`)
+        }
+    }
+    feedT() {
+        console.log("YUMMMM")
+        this.hunger += 2
+        }
+
+    sleepT(){
+        console.log("zzzZZZ")
+        this.sleepiness += 2
+
+    }
+    playT(){
+        console.log("Yayyy")
+        this.boredom += 2
+
+    }
+    ageT(){
+        alert(`Happy Birthday, ${this.name} is 1 year older!`)
+    }
+    }
