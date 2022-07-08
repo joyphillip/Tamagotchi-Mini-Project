0ec88d907ab707bc2245954e3dc03db2e788716c
JPhillip018 <jphillip018@gmail.com>
2022-07-08 18:54:43 -0400

    added set intervals

--- js/app.js
+++ js/app.js
@@ -8,11 +8,11 @@ class Tamagotchi {
         this.boredom = boredom;
         this.age = age;
     }
-    // created methods to decrease stats
+    // created methods 
     feed(){
         console.log('YUMMMMM');
         if (this.hunger >= 10) {
-            stopInterval()
+            stopStatsInterval()
             alert(`${this.name} has died of hunger. GAME OVER!`)
         } else {
             this.hunger +=1
@@ -21,7 +21,7 @@ class Tamagotchi {
     sleep(){
         console.log('zzzZZZ');
         if (this.sleepiness >= 10) {
-            stopInterval()
+            stopStatsInterval()
             alert(`${this.name} has dies of hunger. GAME OVER!`)
         } else {
             this.hunger += 1
@@ -30,8 +30,18 @@ class Tamagotchi {
     play(){
         console.log('Yayyy');
         if( this.boredom >= 10){
-            stopInterval()
+            stopStatsInterval()
             alert(`${this.name} had died of boredom. GAME OVER!`)
+        } else {
+            this.boredom += 1
+        }
+    }
+    ageUp(){
+        console.log('Happy Birthday')
+        this.age += 1
+        if( this.age === 7) {
+            alert(`Nice job! ${this.name} has gotten older! `)
+
         }
     }
 }
@@ -42,7 +52,16 @@ const pet = new Tamagotchi();
 // added event listener to start game
 document.querySelector(".button1").addEventListener('click', function() {
     pet.name = prompt("Give your pet a name!")
-    // console.log(myPet.name)
+    // console.log(pet.name)
+    document.getElementById('name').innerText = pet.name
 })
 
-//
+//Create function to set and change intervals
+function startStatsInterval() {
+let hungerStat = setInterval(pet.hunger, 2000);
+let sleepStat = setInterval(pet.sleep, 1000);
+let playStat = setInterval(pet.sleep, 3000);
+let boredomStat = setInterval(pet.boredom, 1500);
+}
+
+//Create function to stop interbals
